d7c77d5526a3345640a61bf7c702e28e2629e29e
DemetriceW20 <demetrice_williams@hotmail.com>
2021-04-01 08:19:15 -0800

    Built contest_list to allow for specific notification of sweepstake results

--- contestants.py
+++ contestants.py
@@ -7,11 +7,26 @@ class Contestants:
         self.last_name = last_name
         self.email = email
         self.registration = registration
+        self.contest_list = []
+
+    def contestants_lst(self):
+        self.contest_list.append(self)
 
     def notify_winner(self, winner):
-        user_interface.winner_statement(winner)
-        user_interface.winner_statement_contestants(winner)
+        for contestant in self.contest_list:
+            if winner == contestant:
+                user_interface.winner_statement(winner)
+            else:
+                user_interface.winner_statement_contestants(winner)
 
-# contestant1 = Contestants('John', 'Doe', 'jd@gmail.com', 5555)
-# contestant2 = Contestants('Alex', 'Smith', 'as@gmail.com', 4444)
-# contestant3 = Contestants('Ryan', 'Johnson', 'rj@gmail.com', 3333)
+
+contestant1 = Contestants('John', 'Doe', 'jd@gmail.com', 5555)
+contestant2 = Contestants('Alex', 'Smith', 'as@gmail.com', 4444)
+contestant3 = Contestants('Ryan', 'Johnson', 'rj@gmail.com', 3333)
+contestant1.contestants_lst()
+contestant2.contestants_lst()
+contestant2.contestants_lst()
+
+
+
+
